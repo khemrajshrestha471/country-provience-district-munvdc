dcb712b4b7becff2db69cdcbdad5eda3cc5c9c0c
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-04-12 16:41:07 +0545

    first step to push country list only

---
+++ src/components/CountryList/CountryList.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import countries from "../../assets/countries.json";
+
+interface Country {
+    country_name: string; // defining types of country_name for typescript
+}
+
+const CountryList = () => {
+    const [selectedCountry, setSelectedCountry] = useState<string>(""); // state variable to store selected country value
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => { // this function trigger when value select in select field
+        setSelectedCountry(event.target.value);
+    };
+
+    return (
+        <div>
+            <select value={selectedCountry} onChange={handleChange}>
+                <option value="">Country</option>
+                {countries.map((country: Country) => (
+                    <option key={country.country_name} value={country.country_name}>
+                        {country.country_name}
+                    </option>
+                ))}
+            </select>
+            {/* {selectedCountry && <p>Selected country: {selectedCountry}</p>} */}
+        </div>
+    );
+};
+
+export default CountryList;
