687fba9c96fcf75c9714a0d052330ed161a78b0f
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-04-12 17:35:14 +0545

    added rollup file which created dist folder

---
+++ dist/esm/types/components/CountryList/CountryList.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const CountryList: () => React.JSX.Element;
+export default CountryList;
---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import json from '@rollup/plugin-json'; 
+
+// import terser from '@rollup/plugin-terser'
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+// const packageJson = require('./package.json')
+// import packageJson from './package.json';
+import packageJson from './package.json' assert { type: "json" };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+    
+    //   peerDepsExternal(),
+
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      json()
+    //   terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    // external: [/\.css$/],
+  }
+];
